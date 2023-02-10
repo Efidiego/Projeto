8238154e2af0aee6a78c97ad9281ece45499a6ab
Fernando Diego <123587574+Efidiego@users.noreply.github.com>
2023-02-10 10:36:34 -0300

    alteração correção

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function toggleMode() {
-  const html = document.getElementById("light")
-  html.classList.toggle("light")
+  const html = document.getElementById('light')
+  html.classList.toggle('light')
 
-  const img = document.querySelector("#profile img")
+  const img = document.querySelector('#profile img')
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar_escuro.png")
+    img.setAttribute('src', './Assets/avatar_escuro.png')
   } else {
-    img.setAttribute("src", "./assets/avatar_claro.png")
+    img.setAttribute('src', './Assets/avatar_claro.png')
   }
 }
